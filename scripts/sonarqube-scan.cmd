ab3b0d54f6f660719649237cedcc9c77789efad9
Takayuki Maruyama <maruTA-bis5@users.noreply.github.com>
2020-10-25 02:43:22 -0500

    Add missing `sonar:sonar` for windows script

--- scripts/sonarqube-scan.cmd
+++ scripts/sonarqube-scan.cmd
@@ -1,1 +1,1 @@
-mvn clean jacoco:prepare-agent verify jacoco:report -Dmaven.javadoc.failOnError=false -s dev\settings-sonarqube.xml %*
+mvn clean jacoco:prepare-agent verify jacoco:report sonar:sonar -Dmaven.javadoc.failOnError=false -s dev\settings-sonarqube.xml %*
